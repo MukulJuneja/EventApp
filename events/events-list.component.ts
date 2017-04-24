19ad2c38264798dd2deb03f01c32b8fc4f89be7f
Mukul Juneja <mkljuneja18@gmail.com>
2017-04-24 18:32:07 +0530

    24/04/17_6pm

---
+++ events/events-list.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+@Component({
+selector: 'event-list',
+  template:  `
+    <div class="container">
+      <h1>Upcoming Angular 2 Events</h1>
+      <hr />
+    </div>
+  `
+})
+export class EventsListComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [AppComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
